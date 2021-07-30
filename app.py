1f635e8bb84aca0e3fcef986acb0809e9bcc20a1
Faisal Ahmed <faisalbegins@gmail.com>
2021-07-30 13:07:45 -0500

    added flash cache

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_caching import Cache
+
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "RedisCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 
 app = Flask(__name__)
 
+app.config.from_mapping(config)
+cache = Cache(app)
+
 
 @app.route('/')
+@cache.cached(timeout=50)
 def hello_world():
     return 'Hello World!'
 
